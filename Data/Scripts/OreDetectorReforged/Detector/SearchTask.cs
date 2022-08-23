8733e8ee9df838745d543abfb43afb7283720768
S Saukh <seasaukh@rt-dc.ru>
2022-08-23 18:24:48 +0300

    Show markers from nearest and (or) biggest range broadcasting ore detector

--- Data/Scripts/OreDetectorReforged/Detector/SearchTask.cs
+++ Data/Scripts/OreDetectorReforged/Detector/SearchTask.cs
@@ -7,23 +7,26 @@ namespace OreDetectorReforged.Detector
     class SearchTask
     {
         public BoundingSphereD area;
+        public Vector3D center; 
         public string minedOre;
         public Func<Vector3D, bool> resultCb;
         public Action finishCb;
         public List<IDetectorPage> pages;
 
-        public SearchTask(BoundingSphereD area, string minedOre, Func<Vector3D, bool> resultCb, Action finishCb)
+        public SearchTask(BoundingSphereD area, Vector3D center, string minedOre, Func<Vector3D, bool> resultCb, Action finishCb)
         {
             this.area = area;
+            this.center = center;
             this.minedOre = minedOre;
             this.resultCb = resultCb;
             this.finishCb = finishCb;
         }
 
-        public SearchTask(BoundingSphereD area, string minedOre, int count, Action<List<Vector3D>> finishCb)
+        public SearchTask(BoundingSphereD area, Vector3D center, string minedOre, int count, Action<List<Vector3D>> finishCb)
         {
             var results = new List<Vector3D>(count);
             this.area = area;
+            this.center = center;
             this.minedOre = minedOre;
             this.resultCb = (v) =>
             {
@@ -33,11 +36,11 @@ namespace OreDetectorReforged.Detector
             this.finishCb = () => finishCb(results);
         }
 
-        public SearchTask(ValueTuple<BoundingSphereD, string, Func<Vector3D, bool>, Action> t) : this(t.Item1, t.Item2, t.Item3, t.Item4)
+        public SearchTask(ValueTuple<BoundingSphereD, Vector3D, string, Func<Vector3D, bool>, Action> t) : this(t.Item1, t.Item2, t.Item3, t.Item4, t.Item5)
         {
         }
 
-        public SearchTask(ValueTuple<BoundingSphereD, string, int, Action<List<Vector3D>>> t) : this(t.Item1, t.Item2, t.Item3, t.Item4)
+        public SearchTask(ValueTuple<BoundingSphereD, Vector3D, string, int, Action<List<Vector3D>>> t) : this(t.Item1, t.Item2, t.Item3, t.Item4, t.Item5)
         {
         }
     }
--- Data/Scripts/OreDetectorReforged/PlayerOreDetectorSession.cs
+++ Data/Scripts/OreDetectorReforged/PlayerOreDetectorSession.cs
@@ -14,6 +14,44 @@ namespace OreDetectorReforged
         int counter;
         List<int>[] gpss;
 
+        HashSet<IMyCubeGrid> grids;
+
+        public override void BeforeStart()
+        {
+            grids = new HashSet<IMyCubeGrid>();
+            HashSet<IMyEntity> allEnts = new HashSet<IMyEntity>();
+            MyAPIGateway.Entities.GetEntities(allEnts, (a) => {
+                Entities_OnEntityAdd(a);
+                return false;
+            });
+
+            MyAPIGateway.Entities.OnEntityAdd += Entities_OnEntityAdd;
+            MyAPIGateway.Entities.OnEntityRemove += Entities_OnEntityRemove;
+        }
+
+        private void Entities_OnEntityAdd(IMyEntity obj)
+        {
+            if(obj is IMyCubeGrid)
+            {
+                grids.Add(obj as IMyCubeGrid);
+            }
+        }
+
+        private void Entities_OnEntityRemove(IMyEntity obj)
+        {
+            if(grids.Contains(obj as IMyCubeGrid))
+            {
+                grids.Remove(obj as IMyCubeGrid);
+            }
+        }
+
+
+        protected override void UnloadData()
+        {
+            MyAPIGateway.Entities.OnEntityAdd -= Entities_OnEntityAdd;
+            MyAPIGateway.Entities.OnEntityRemove -= Entities_OnEntityRemove;
+        }
+
         public override void UpdateAfterSimulation()
         {
             var period = MaterialMappingHelper.Static.naturalOres.Length * SyncSessionComponent.detFrequencyDivider;
@@ -39,7 +77,7 @@ namespace OreDetectorReforged
             var settings = TerminalOreDetector.GetStorage(detector);
             var ore = MaterialMappingHelper.Static.naturalOres[o];
             var area = new BoundingSphereD(detector.GetPosition(), settings.range);
-            DetectorServer.Add(new SearchTask(area, ore, settings.count, (results) =>
+            DetectorServer.Add(new SearchTask(area, MyAPIGateway.Session.Player.Character.GetPosition(), ore, settings.count, (results) =>
             {
                 RemoveMarkers(o);
                 if (MyAPIGateway.Gui.GetCurrentScreen == MyTerminalPageEnum.None)
@@ -56,17 +94,45 @@ namespace OreDetectorReforged
         IMyOreDetector GetDetector(int o)
         {
             var shipController = Session.ControlledObject as IMyShipController;
-            if (shipController == null)
-                return null;
             var maxrange = 1.5f;
+            var minDistance = -1d;
             IMyOreDetector det = null;
-            foreach (var detector in shipController.CubeGrid.GetFatBlocks<IMyOreDetector>())
+            if (shipController != null)
             {
-                var settings = TerminalOreDetector.GetStorage(detector);
-                if (!detector.IsWorking || settings == null || !settings.Whitelist[o] || settings.range < maxrange)
-                    continue;
-                maxrange = settings.range;
-                det = detector;
+                foreach (var detector in shipController.CubeGrid.GetFatBlocks<IMyOreDetector>())
+                {
+                    var settings = TerminalOreDetector.GetStorage(detector);
+                    if (!detector.IsWorking || settings == null || !settings.Whitelist[o] || settings.range < maxrange)
+                        continue;
+                    maxrange = settings.range;
+                    det = detector;
+                }
+            }
+
+            if (det == null)
+            {
+                Vector3D playerPos = MyAPIGateway.Session.Player.Character.GetPosition();
+                foreach(var grid in grids)
+                {
+                    foreach (var detector in grid.GetFatBlocks<IMyOreDetector>())
+                    {
+                        var settings = TerminalOreDetector.GetStorage(detector);
+                        var distance = Vector3D.DistanceSquared(detector.GetPosition(), playerPos);
+                        if (!detector.IsWorking || !detector.BroadcastUsingAntennas || !detector.HasPlayerAccess(MyAPIGateway.Session.Player.IdentityId) || settings == null || !settings.Whitelist[o] || (minDistance > 0 && (settings.range - distance) < (maxrange - minDistance)))
+                            continue;
+
+                        foreach (var ant in grid.GetFatBlocks<IMyRadioAntenna>())
+                        {
+                            if (ant.IsWorking && ant.IsBroadcasting && ant.HasPlayerAccess(MyAPIGateway.Session.Player.IdentityId) && distance < ant.Radius*ant.Radius)
+                            {
+                                minDistance = distance;
+                                maxrange = settings.range;
+                                det = detector;
+                                break;
+                            }
+                        }
+                    }
+                }
             }
             return det;
         }
